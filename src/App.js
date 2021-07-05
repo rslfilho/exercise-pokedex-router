5fdfd2e5feffcf1b7f25e5830c508d5cc66deaa6
Roberval Filho <rslfilho@gmail.com>
2021-07-05 19:31:22 -0300

    Criando rota para Pokedex

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
@@ -6,8 +7,10 @@ import Pokedex from './Pokedex';
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
+      <Route path="/" render={ (props) => <Pokedex {...props} pokemons={pokemons} /> } />
+      </BrowserRouter>
     </div>
   );
 }
